247e0beb334b9b9619db37e9727260d128ff3123
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-08-14 12:33:02 -0400

    Reconfigure application structure

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/app')
-
-const port = process.env.PORT || 3000
-
-app.listen(port,function(){
-  console.log(`Running on localhost:${port}`)
-})
--- src/app.js
+++ src/app.js
@@ -1,21 +1,44 @@
+const createError = require('http-errors')
 const express = require('express')
-const bodyParser = require('body-parser')
+const logger = require('morgan')
+
 const cors = require('cors')
 
 const pingRoute = require('./routes/ping')
 
 const app = express()
 
+app.use(logger('dev'))
+app.use(express.json())
+
 app.use(cors())
 app.options('*', cors())
 
-app.use(bodyParser.json())
-
 app.use('/ping', pingRoute)
 
-// This is a route only to know if the application is running properly
 app.get('/',function(req,res){
   res.send('Sitewatch API')
 })
+
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+
+  const error = {
+    message: err.message || 'Internal Error',
+    status: err.status || 500
+  }
+
+  res.json(error)
+})
+
 
 module.exports = app
